74febf6c5f955830527734e4f877e10daaca7b63
helenvbrnva <114693381+helenvbrnva@users.noreply.github.com>
2025-06-28 00:46:21 +0300

    Add files via upload

---
+++ src/pages/user/UserMainPage.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: "UserMainPage"
+}
+</script>
+
+<template>
+  <v-main>
+    <router-view>
+
+    </router-view>
+  </v-main>
+  <v-footer padless color="#222">
+    <v-col class="text-center white--text py-6">
+      <div class="text-h6 font-weight-bold">SMOM — забота с первых шагов</div>
+      <div class="text-caption mt-2">© 2025 SMOM. Все права защищены.</div>
+    </v-col>
+  </v-footer>
+</template>
+
+<style scoped>
+
+</style>
