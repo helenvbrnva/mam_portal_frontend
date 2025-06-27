b58b2cd94fa93555b9e5d732149baa050ab171e7
helenvbrnva <114693381+helenvbrnva@users.noreply.github.com>
2025-06-28 00:42:35 +0300

    Add files via upload

---
+++ src/pages/admin/AdminMainPage.vue
@@ -0,0 +1,21 @@
+<script>
+import AdminHeader from "@/components/AdminHeader.vue";
+
+export default {
+  name: "AdminMainPage",
+  components: {AdminHeader}
+}
+</script>
+
+<template>
+  <admin-header/>
+  <v-main>
+    <router-view>
+
+    </router-view>
+  </v-main>
+</template>
+
+<style scoped>
+
+</style>
